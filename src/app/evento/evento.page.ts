cfdf5334488b2f937049a4deb896b99623e61769
Amancayjesus136 <amancayjesus136@gmail.com>
2023-10-27 19:06:58 -0500

    evento.ts

--- src/app/evento/evento.page.ts
+++ src/app/evento/evento.page.ts
@@ -17,20 +17,12 @@ export class EventoPage implements OnInit {
     ) {
         this.route.params.subscribe(params => {
           const evento_id = params['id'];
-    
-    /* LLAMAMOS NUEVAMENTE A TODO EL JSON DE EVENTOS*/
 
-    this.http.get<any>('assets/eventos.json')
-      .subscribe(data => {
-
-        const results = data.eventos.filter((obj: {id: string;}) => {
-          return obj.id === evento_id;
-        });
-      console.log(results[0])
-      this.evento = results[0];
-      })
-
-    /* LLAMAMOS NUEVAMENTE A TODO EL JSON DE EVENTOS*/
+          this.http.get<any>('http://localhost:3000/eventos/detalle/'
+          + evento_id)
+          .subscribe(data => {
+            console.log('eventos', data);
+          })
   });
 }
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -21,7 +21,7 @@ export class Tab1Page {
   }
 
   verdetalle(evento: any){
-    this.route.navigate(['/evento/'+evento.id])
+    this.route.navigate(['/evento/'+evento._id])
   }
 
 }
