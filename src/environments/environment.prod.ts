b6f63eb4be4f57e8e203cf6d35da8bc71efc3443
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-24 21:58:13 -0500

    push enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDmKMnQA5igD-mO46S1mi1qHE4OKo3jAq0",
+    authDomain: "llegoapp-ba6c2.firebaseapp.com",
+    projectId: "llegoapp-ba6c2",
+    storageBucket: "llegoapp-ba6c2.appspot.com",
+    messagingSenderId: "1073063355237",
+    appId: "1:1073063355237:web:f7a5833b057e5f8665b78c",
+    measurementId: "G-XDV42XGQDR"
+    }
 };
